8664005df772ffc3969663f3f57a010198ee5833
rws205 <rws205@gmail.com>
2018-04-13 15:23:54 +0800

    add detect signal function

--- UAV205/app/src/main/java/com/a205/mpc/uav_205/MainActivity.java
+++ UAV205/app/src/main/java/com/a205/mpc/uav_205/MainActivity.java
@@ -1,12 +1,17 @@
 package com.a205.mpc.uav_205;
 
+import android.content.DialogInterface;
 import android.content.pm.ActivityInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 import android.widget.VerticalSeekBar;
 
@@ -16,12 +21,19 @@ import java.net.Socket;
 //socket: https://www.jianshu.com/p/089fb79e308b
 
 public class MainActivity extends AppCompatActivity {
+    private static final int MIN_RSSI = -100;
+    private static final int MAX_RSSI = -55;
     private Socket clientSocket = null;
     private ImageButton btn_lock;
     private ImageButton btn_connect;
+    private ImageView   icon_signal;
     private boolean isLock = true;
     private boolean isConnect = false;
     private VerticalSeekBar seekBar;
+    private WifiInfo wifiInfo;
+    private Thread monitor;
+    private int[] signal_lv = {R.drawable.signal_lv0, R.drawable.signal_lv1,
+            R.drawable.signal_lv2, R.drawable.signal_lv3, R.drawable.signal_lv4};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +53,63 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
         btn_connect = (ImageButton)findViewById(R.id.btn_connect);
-        btn_lock = (ImageButton)findViewById(R.id.btn_lock);
-        seekBar = (VerticalSeekBar)findViewById(R.id.vertical_Seekbar);
+        btn_lock    = (ImageButton)findViewById(R.id.btn_lock);
+        seekBar     = (VerticalSeekBar)findViewById(R.id.vertical_Seekbar);
+        icon_signal = (ImageView)findViewById(R.id.signal);
+        WifiManager mWifiManager = (WifiManager)this.getSystemService(this.WIFI_SERVICE);
+        wifiInfo = mWifiManager.getConnectionInfo();
 
+        if (!"UAV205".equals(wifiInfo.getSSID()))
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Incorrect SSID");
+            builder.setMessage("Please select correct SSID.");
+            builder.create();
+            AlertDialog dialog = builder.create();
+            dialog.show();
+            dialog.dismiss();
+        }
+
+        monitor = new Thread(new Runnable(){
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        WifiManager mWifiManager = (WifiManager)MainActivity.this.getSystemService(MainActivity.WIFI_SERVICE);
+                        wifiInfo = mWifiManager.getConnectionInfo();
+                        final int level = calculateSignalLevel(wifiInfo.getRssi(), 4);
+                        Log.e("UAV Rssi:", Integer.toString(level));
+                        runOnUiThread(new Runnable() {        //可以使用此方法臨時交給UI做顯示
+                            public void run() {
+                                icon_signal.setImageResource(signal_lv[level]);
+                            }
+
+                        });
+
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        monitor.start();
+        Toast.makeText(getApplicationContext(), wifiInfo.getSSID()+"RSSI:"+Integer.toString(wifiInfo.getRssi()), Toast.LENGTH_SHORT).show();
+    }
+
+    public static int calculateSignalLevel(int rssi, int numLevels) {
+    /* in general, numLevels is 4  */
+        if (rssi <= MIN_RSSI) {
+            return 0;
+        } else if (rssi >= MAX_RSSI) {
+            return numLevels - 1;
+        } else {
+            float inputRange = (MAX_RSSI - MIN_RSSI);
+            float outputRange = (numLevels - 1);
+
+            return (int)((float)(rssi - MIN_RSSI) * outputRange / inputRange);
+        }
     }
 
     public static byte[] command(byte act, byte val)
